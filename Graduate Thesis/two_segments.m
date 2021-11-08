70f6d768e6be550162fe7dc10895f223e9b9fe13
yuto-ohigashi <59161369+yuto-ohigashi@users.noreply.github.com>
2021-11-09 01:38:52 +0900

    2011_11_09_0138

--- Graduate Thesis/two_segments.m
+++ Graduate Thesis/two_segments.m
@@ -9,27 +9,26 @@ cen_foot = 0.595;
 cen_low = 0.406;
 mc_foot_x = 19-len_foot*cen_foot; %% 19はつま先からくるぶしまでの距離
 mc_foot_y = 0; 
-mc_low_x = 0;
 mc_low_y = len_low*(1-cen_low);
 %% 足関節の可動域
 theta_ank = 1:1:30;
 %% 足関節角度に対する下腿の質量中心の座標
-g_lowleg = zeros(length(theta_ank),2);
+g_low = zeros(length(theta_ank),2);
 for i = 1:length(theta_ank)
-    x_lowleg = mc_low_y * sin(theta_ank(i));
-    g_lowleg(i,1) = x_lowleg;
+    x_low = mc_low_y * sin(theta_ank(i));
+    g_low(i,1) = x_low;
 end
 for i = 1:length(theta_ank)
-    y_lowleg = mc_low_y * cos(theta_ank(i));
-    g_lowleg(i,2) = y_lowleg;
+    y_low = mc_low_y * cos(theta_ank(i));
+    g_low(i,2) = y_low;
 end
 %% 足関節角度に対する下腿と足の質量重心
 g = zeros(length(theta_ank),2);
 for j = 1:length(theta_ank)
-    x_g = (m_foot*g_lowleg(j,1) + m_low*mc_low_x)/(m_foot+m_low);
+    x_g = (m_foot*mc_foot_x + m_low*g_low(j,1))/(m_foot+m_low);
     g(j,1) = x_g;
 end
 for j = 1:length(theta_ank)
-    y_g = (m_foot*g_lowleg(j,2) + m_low*mc_low_y)/(m_foot+m_low);
+    y_g = (m_foot*mc_foot_y + m_low*g_low(j,2))/(m_foot+m_low);
     g(j,2) = y_g;
 end
