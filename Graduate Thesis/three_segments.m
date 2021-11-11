49a03e6a23d4fdc8d49458f9a43bcfad333372dd
yuto-ohigashi <59161369+yuto-ohigashi@users.noreply.github.com>
2021-11-11 19:36:08 +0900

    2021_11_11_1936

--- Graduate Thesis/three_segments.m
+++ Graduate Thesis/three_segments.m
@@ -1,13 +1,15 @@
 %% 足部と下腿の質量を決定(論文1の体重を利用)
 m_foot = 69.6*1.1/100;
 m_low = 69.6*5.1/100;
-m_femur = 69.6*11.0/100;
+m_femur = 69.6*11/100;
+
 %% 足部と下腿の部分長(自分の長さ)
 len_foot = 25.5;
 len_low = 47.0;
 len_femur = 41.0;
-% 問題点：セグメントの部分長の決定をどうするか
-%% 足部と下腿の質量中心(直立時の距骨と脛骨の接合部(くるぶし？自分の長さ)を原点Oとした座標)、質量中心(cf,cl)の位置は上端からの比
+% 問題点：セグメントの長さの決定をどうするか
+
+%% 足部と下腿の質量中心(直立時の距骨と脛骨の接合部(くるぶし？自分の長さ)を原点Oとした座標)、質量中心(cf,cl,cfe)の位置は上端からの比
 cen_foot = 0.595;
 cen_low = 0.406;
 cen_femur = 0.475;
@@ -18,17 +20,33 @@ mc_low_y = len_low*(1-cen_low);
 mc_femur_x = 0;
 mc_femur_y = len_low + len_femur*(1-cen_femur);
 % 問題点：原点をどこにするか
+
 %% 関節の可動域
+% 足関節角度は直立時の下腿の線と屈曲した時の下腿の線の間の角度
+% 膝関節角度は屈曲前の膝が真っ直ぐになっている時の大腿の線と屈曲した時の大腿の線の間の角度
 theta_ank = 0:0.01:pi/6;
-theta_ank = 0:0.01:13*pi/18;
-%% 関節角度に対する各セグメントの質量重心
-g_low = zeros(length(theta_ank)*;length,2);
+theta_knee = 0:0.01:13*pi/18;
+
+%% 各関節角度に対する各セグメントの質量中心の座標
+g_low_femur = zeros(length(theta_ank)*length(theta_knee),6);
+% gは順番に(足関節角度 膝関節角度 下腿の質量中心のx座標 下腿の質量中心のy座標 大腿の質量中心のx座標 大腿の質量中心のy座標)
 for i = 1:length(theta_ank)
-    x_low = mc_low_y * sin(theta_ank(i));
-    g_low(i,1) = x_low;
+    x_low = mc_low_y * sin(theta_ank(i)); %下腿の質量中心のx座標を(下腿の長さ)*sinθで計算
+    y_low = mc_low_y * cos(theta_ank(i)); %下腿の質量中心のy座標を(下腿の長さ)*cosθで計算
+    for j = 1:length(theta_knee)
+        femur
+        x_femur = ;
+        y_femur = ;
+        g_low_femur(j*(i-1)+j,:) = [theta_ank(i) theta_ank(j) x_low y_low x_femur y_femur];
+    end
 end
-for i = 1:length(theta_ank)
-    y_low = mc_low_y * cos(theta_ank(i));
-    g_low(i,2) = y_low;
-end
-%% 足関節と膝関節角度に対する
+
+%% 各関節角度に対する重心座標
+g = zeros(length(theta_ank)*length(theta_knee),4);
+% gは順番に(足関節角度 膝関節角度 重心のx座標 重心のy座標)
+
+
+%% 重心が足関節内にあるかの判定
+squat_position = zeros(length(theta_ank),4);
+% squat_positionは順番に(足関節角度 膝関節角度 重心のx座標 重心のy座標)
+
--- Graduate Thesis/two_segments.m
+++ Graduate Thesis/two_segments.m
@@ -1,10 +1,12 @@
 %% 足部と下腿の質量を決定(論文1の体重を利用)
 m_foot = 69.6*1.1/100;
 m_low = 69.6*5.1/100;
+
 %% 足部と下腿の部分長(自分の長さ)
 len_foot = 25.5;
 len_low = 47.0;
-% 問題点：セグメントの部分長の決定をどうするか
+% 問題点：セグメントの長さの決定をどうするか
+
 %% 足部と下腿の質量中心(直立時の距骨と脛骨の接合部(くるぶし？自分の長さ)を原点Oとした座標)、質量中心(cf,cl)の位置は上端からの比
 cen_foot = 0.595;
 cen_low = 0.406;
@@ -13,26 +15,32 @@ mc_foot_y = 0;
 mc_low_x = 0;
 mc_low_y = len_low*(1-cen_low);
 % 問題点：原点をどこにするか
+
 %% 足関節の可動域
+% 足関節角度は直立時の下腿の線と屈曲した時の下腿の線の間の角度
 theta_ank = 0:0.01:pi/9;
+
 %% 足関節角度に対する下腿の質量中心の座標
 g_low = zeros(length(theta_ank),3);
 % g_lowは順番に(足関節角度 下腿の質量中心のx座標 下腿の質量中心のy座標)
 for i = 1:length(theta_ank)
-    x_low = mc_low_y * sin(theta_ank(i));
-    y_low = mc_low_y * cos(theta_ank(i));
+    x_low = mc_low_y * sin(theta_ank(i)); %下腿の質量中心のx座標を(下腿の長さ)*sinθで計算
+    y_low = mc_low_y * cos(theta_ank(i)); %下腿の質量中心のy座標を(下腿の長さ)*cosθで計算
     g_low(i,:) = [theta_ank(i) x_low y_low];
 end
-%% 足関節角度に対する下腿と足の質量重心
+
+%% 足関節角度に対する下腿と足の重心
 g = zeros(length(theta_ank),3);
 % gは順番に(足関節角度 重心のx座標 重心のy座標)
 for j = 1:length(theta_ank)
-    x_g = (m_foot*mc_foot_x + m_low*g_low(j,2))/(m_foot+m_low);
-    y_g = (m_foot*mc_foot_y + m_low*g_low(j,3))/(m_foot+m_low);
+    x_g = (m_foot*mc_foot_x + m_low*g_low(j,2))/(m_foot+m_low); %重心のx座標
+    y_g = (m_foot*mc_foot_y + m_low*g_low(j,3))/(m_foot+m_low); %
     g(j,:) = [theta_ank(j) x_g y_g];
 end
+
 %% 重心が足関節内にあるかの判定
 squat_position = zeros(length(theta_ank),3);
+% squat_positionは順番に(足関節角度 重心のx座標 重心のy座標)
 for k = 1:length(theta_ank)
     if g(k,2) < 19 && g(k,2) > -6.5
         squat_position(k,:) = g(k,:);
